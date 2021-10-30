10cdf20fa54fd63de4bfcd120873fdbff0e9bb9e
Alice <hi@dnydxn.moe>
2021-10-30 03:22:48 +0000

    convert to arrow fns, parse search engines

--- new.js
+++ new.js
@@ -2,20 +2,49 @@ const term = document.getElementById("term");
 const interpret = document.getElementById("interpret");
 const prompt = document.getElementById("prompt");
 
-prompt.addEventListener("input", input);
-prompt.addEventListener("keydown", is_enter);
+const CH_ENGINE = '@';
 
-function input() {
-    console.log(prompt.value);
-    // todo
-}
+const input = () => {
+    interpret_update(prompt.value);
+    console.log(parse(prompt.value));
+};
 
-function is_enter() {
+const is_enter = () => {
     if (event.keyCode == 13) {
 	alert("awa");
 	// todo
     }
-}
+};
+
+const interpret_update = (input) => {
+    if (input !== "") {
+	interpret.innerHTML = input;
+    } else {
+	interpret.innerHTML = "&nbsp;";
+    }
+};
+
+const parse = (input) => {
+    const match = (new RegExp(`^ *((?:${CH_ENGINE}.*? +)+)(.*?) *$`)).exec(input);
+    if (match) {
+	return { engines: parse_engines(match[1]), query: match[2] };
+    } else {
+	return { engines: [""], query: input };
+    }
+};
+
+const parse_engines = (match) =>
+      [...new Set(
+	match
+	    .split(" ")
+	    .filter(x => x)
+	    .map(x => x.substr(CH_ENGINE.length))
+      )]; 
 
 const faces = ["nwn", "=w=", "~w~", "uwu", ">w<", ".w."];
 prompt.placeholder = faces[Math.floor(Math.random() * faces.length)];
+
+prompt.addEventListener("input", input);
+prompt.addEventListener("keydown", is_enter);
+
+interpret_update("");
