cabb8e1a70f3d5adcc7f3845d944399eea2dd0c1
Alice <hi@dnydxn.moe>
2021-10-31 07:10:16 +0000

    nicer terminal scaling
    the terminal will now scale vertically in steps of the font size, and
    will disappear when it becomes too small

--- new.js
+++ new.js
@@ -2,6 +2,8 @@ const term = document.getElementById("term");
 const interpret = document.getElementById("interpret");
 const prompt = document.getElementById("prompt");
 
+const unit_font = parseInt(window.getComputedStyle(prompt).fontSize.slice(0, -2));
+
 const CH_ENGINE = '@';
 
 const input = () => {
@@ -15,6 +17,25 @@ const is_enter = () => {
 	// todo
     }
 };
+
+const window_resize = () => {
+    const h = window.innerHeight;
+    const lines = Math.floor(0.4 * h / unit_font) - 2;
+    if (lines > 0) {
+	term.style.display = "initial";
+	interpret.style.display = "initial";
+	term.style.height = (lines * unit_font) + "px";
+    } else {
+	term.style.display = "none";
+	if (lines <= -1) {
+	    interpret.style.display = "none";
+	}
+    }
+};
+
+prompt.addEventListener("input", input);
+prompt.addEventListener("keydown", is_enter);
+window.onresize = window_resize;
 
 const interpret_update = (input) => {
     if (input !== "") {
@@ -34,17 +55,15 @@ const parse = (input) => {
 };
 
 const parse_engines = (match) =>
-      [...new Set(
+    [...new Set(
 	match
 	    .split(" ")
 	    .filter(x => x)
 	    .map(x => x.substr(CH_ENGINE.length))
-      )]; 
+    )]; 
 
 const faces = ["nwn", "=w=", "~w~", "uwu", ">w<", ".w."];
 prompt.placeholder = faces[Math.floor(Math.random() * faces.length)];
 
-prompt.addEventListener("input", input);
-prompt.addEventListener("keydown", is_enter);
-
 interpret_update("");
+window_resize();
