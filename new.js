48e30b69a6f95ed780d1908cebc5f3200775faa0
Alice <hi@dnydxn.moe>
2021-11-04 18:35:11 +0000

    rewrite parsing to accommodate bookmarks
    squashed with: term display, use classes for text

--- new.js
+++ new.js
@@ -5,11 +5,24 @@ const prompt = document.getElementById("prompt");
 const html_escape = (str) =>
       str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
 
-const text_colour = (text, col) =>
-      `<span style="color: var(--col-${col});">${text}</span>`;
+const span_class = (text, c) =>
+      `<span class="${c}">${text}</span>`;
 
 const engines_format = (engine) =>
-    engine.name ? `${text_colour(engine.name, "engine")}` : `${text_colour(engine.tag, "unknown")}?`;
+    engine.name ? `${span_class(engine.name, "engine")}` : `${span_class(engine.tag, "unknown")}?`;
+
+const bookmarks_format = (bookmark) =>
+      bookmark.name ? `${span_class(bookmark.name, "bookmark")}` : `${span_class(bookmark.tag, "unknown")}?`;
+
+const engines_get = (tag) => {
+    const s = config_search_engines.find((engine) => engine.tag == tag);
+    return s ? s : { "tag": tag };
+};
+
+const bookmarks_get = (tag) => {
+    const s = config_bookmarks.find((bookmark) => bookmark.tag == tag);
+    return s ? s : { "tag": tag };
+}
 
 const input = () =>
       interpret_update(prompt.value);
@@ -21,50 +34,74 @@ const is_enter = () => {
 };
 
 const parse = (input) => {
-    const match = (new RegExp(`^ *((?:${config_engine_prefix}.*? +)+)(.*?) *$`)).exec(input);
-    if (match) {
-	return { engines: engines_parse(match[1]), query: match[2] };
+    const i = input.trim();
+    if (i[0] == config_bookmark_prefix) {
+	return {
+	    type: "bookmark",
+	    bookmarks: i
+		.split(" ")
+		.filter(x => new RegExp(`^${config_bookmark_prefix}`).exec(x))
+		.map(x => x.slice(config_bookmark_prefix.length))
+	}
     } else {
-	return { engines: [""], query: input };
+	const s = i.split(" ");
+	let idx;
+	for (idx = 0; idx < s.length; idx++) {
+	    if (!(new RegExp(`^${config_engine_prefix}`).exec(s[idx]))) {break;}
+	}
+	const e = [...new Set (
+	    s
+		.slice(0, idx)
+		.map(x => x.slice(config_engine_prefix.length))
+	)];
+	return {
+	    type: "engine",
+	    engines: e.length ? e : [""],
+	    query: s
+		.slice(idx, s.length)
+		.map(x => x.trim())
+		.join(" ")
+	}
     }
 };
 
-const engines_parse = (match) =>
-    [...new Set(
-	match
-	    .split(" ")
-	    .filter(x => x)
-	    .map(x => x.substr(config_engine_prefix.length))
-    )]; 
-
-const engines_get = (tag) => {
-    const s = config_search_engines.find((engine) => engine.tag == tag);
-    return s ? s : { "tag": tag };
-};
-
 const search = (input) => {
-    const parsed = parse(html_escape(input.trim()));
-    parsed
-	.engines
-	.map(engines_get)
-	.map(x => x.url)
-	.filter(x => x)
-	.map(x => x.replace(config_url_replace, parsed.query))
-	.map(x => window.open(x))
-	.map(() => window.close());
+    const parsed = parse(input.trim());
+    if (parsed.type == "engine") {
+	parsed
+	    .engines
+	    .map(engines_get)
+	    .map(x => x.url)
+	    .filter(x => x)
+	    .map(x => (x.replace(config_url_replace, encodeURIComponent(parsed.query))))
+	    .map(x => window.open(x))
+	    .map(() => window.close());
+    } else if (parsed.type == "bookmark") {
+	parsed
+	    .bookmarks
+	    .map(bookmarks_get)
+	    .map(x => x.url)
+	    .filter(x => x)
+	    .map(x => window.open(x))
+	    .map(() => window.close());
+    }
 };
 
 const interpret_update = (input) => {
     const parsed = parse(html_escape(input.trim()));
-    if (parsed.query != "") {
+    if (parsed.type == "engine") {
+	    interpret.innerHTML = parsed
+		.engines
+		.map(engines_get)
+		.map(engines_format)
+		.join(", ")
+		.concat(` ${config_search_char} ${span_class(parsed.query, "query")}`);
+    } else if (parsed.type == "bookmark") {
 	interpret.innerHTML = parsed
-	    .engines
-	    .map(engines_get)
-	    .map(engines_format)
-	    .join(", ")
-	    .concat(` ${config_search_char} ${text_colour(parsed.query, "query")}`);
-    } else {
-	interpret.innerHTML = "&nbsp;";
+	    .bookmarks
+	    .map(bookmarks_get)
+	    .map(bookmarks_format)
+	    .join(", ");
     }
 };
 
@@ -72,7 +109,8 @@ prompt.addEventListener("input", input);
 prompt.addEventListener("keydown", is_enter);
 interpret_update("");
 
-// silly
-const faces = ["nwn", "=w=", "~w~", "uwu", ">w<", ".w."];
-prompt.placeholder = faces[Math.floor(Math.random() * faces.length)];
-term.innerHTML = "1 | this is example text<br> 2 | this is more text...<br> 3 | yup... <br> 4 | um .w. <br> 5 | ... <br> 6 | ... <br> 7 | w";
+prompt.placeholder = config_prompt_placeholders[Math.floor(Math.random() * config_prompt_placeholders.length)];
+
+const bookmarks_text = config_bookmarks.map(x => span_class(`${config_bookmark_prefix}${x.tag}`, "bookmark").concat(span_class(` (${x.name})`, "diminish"))).join(" ");
+const engines_text = config_search_engines .map(x => span_class(`${config_engine_prefix}${x.tag}`, "engine").concat(span_class(` (${x.name})`, "diminish"))) .join(" ");
+term.innerHTML = engines_text.concat("<br><br>").concat(bookmarks_text);
