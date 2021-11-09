909cb4dd58bd65de7091a218478d6b4e4feec107
Alice <hi@dnydxn.moe>
2021-11-09 10:47:14 +0000

    public release!
    =w=

---
+++ config.js
@@ -0,0 +1,62 @@
+// placeholders for the prompt
+const config_prompt_placeholders = ["search..."];
+
+// separator between search engines and query
+const config_search_char = ">"
+
+// prefix for denoting engines in search queries, e.g "@" means "@abc"
+// refers to whichever search engine is given the tag "abc".
+const config_engine_prefix = "@";
+
+// placeholder characters to replace with a query.
+const config_url_replace = "{}";
+
+// your chosen search engines! an array of objects, each object must have:
+// a 'tag' by which you refer to it, a 'name' which will be displayed, and
+// a 'url' to open, containing `config_url_replace` where the query is to be substituted.
+// a default search engine must have an empty string as a tag.
+const config_search_engines = [
+    {
+	name: "default",
+	tag: "",
+	url: "https://duckduckgo.com/?q={}"
+    },
+    {
+	name: "github",
+	tag: "gh",
+	url: "https://github.com/search?q={}"
+    },
+    {
+	name: "google",
+	tag: "go",
+	url: "https://www.google.co.uk/search?q={}"
+    },
+    {
+	name: "wikipedia",
+	tag: "wiki",
+	url: "https://www.wikipedia.org/wiki/{}"
+    },
+    {
+	name: "youtube",
+	tag: "yt",
+	url: "https://www.youtube.com/results?search_query={}"
+    }
+];
+
+// same as for search engines, but for bookmarks
+const config_bookmark_prefix = "#";
+
+// your bookmarks! an array of objects, each object must have:
+// a 'tag' by which you refer to it, a 'name' which will be displayed, and a 'url' to open.
+const config_bookmarks = [
+    {
+	name: "weather",
+	tag: "w",
+	url: "https://wttr.in"
+    },
+    {
+	name: "xkcd",
+	tag: "xkcd",
+	url: "https://xkcd.com"
+    }
+];
